c794f9985284fadd0a07abf5ff5d3cb8f6cfa18f
Md Zubair Beg <ziibii88@gmail.com>
2021-06-04 13:16:16 +0800

    added login api endpoint

--- backend/common/urls.py
+++ backend/common/urls.py
@@ -4,4 +4,5 @@ from backend.common import views
 
 urlpatterns = [
     path('register/', views.RegisterAPIView.as_view(), name='register'),
+    path('login/', views.LoginAPIView.as_view(), name='login'),
 ]
--- backend/common/views.py
+++ backend/common/views.py
@@ -1,5 +1,7 @@
 from logging import getLogger
 
+from django.contrib.auth import get_user_model
+
 from rest_framework import exceptions
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -12,7 +14,7 @@ logger = getLogger(__name__)
 class RegisterAPIView(APIView):
 
     def post(self, request):
-        """POST method"""
+        """POST method for Register"""
         data: dict = request.data
 
         # check if passwords match
@@ -26,5 +28,24 @@ class RegisterAPIView(APIView):
         ser = UserSerializer(data=data)
         ser.is_valid(raise_exception=True)  # raises ValidationError
         ser.save()
+
+        return Response(data=ser.data)
+
+
+class LoginAPIView(APIView):
+
+    def post(self, request):
+        """POST method for Login"""
+        data: dict = request.data
+        # get the user
+        user = get_user_model().objects.filter(email=data.get('email')).first()
+        # check if user exists
+        if user is None:
+            raise exceptions.AuthenticationFailed('User not found!')
+        # check for correct password
+        if not user.check_password(data.get('password')):
+            raise exceptions.AuthenticationFailed('Incorrect password!')
+        # serialize the user object
+        ser = UserSerializer(user)
 
         return Response(data=ser.data)
